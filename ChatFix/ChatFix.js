44caf479d30f83898812bc4b1efdca74896c8b77
Skippidar <Skippidar@hotmail.com>
2014-05-17 08:52:27 +0400

    Aesthetic correction
    Aesthetic correction

--- ChatFix/ChatFix.js
+++ ChatFix/ChatFix.js
@@ -4,14 +4,17 @@ function On_Chat(Player, ChatMessage) {
 	ChatMessage.NewText = "";
 	var string = "";
 	if (ChatString.length > 47){
+		var Posted = false;
+		var Name = Player.Name;
 		for (var i = 1; i <= ChatString.length - 2; i++){
 			string = string + ChatString.charAt(i);
 			if (((string.length > 35) && (string.charAt(string.length) == " ")) || (string.length > 45)){
-				Server.BroadcastFrom(Player.Name, string);
+				Server.BroadcastFrom(Name, string);
+				Name = ":";
 				string = "";
 			}
 		}	
-		Server.BroadcastFrom(Player.Name, string);	
+		Server.BroadcastFrom(Name, string);	
 	}
 	return;
 }
